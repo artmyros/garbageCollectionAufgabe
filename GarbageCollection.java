56da0a3f9b4c13eb083927992ba19ccdae81db04
Myroslav Artiukh <49280881+artmyros@users.noreply.github.com>
2025-02-14 15:18:28 +0100

    Update GarbageCollection.java

--- GarbageCollection.java
+++ GarbageCollection.java
@@ -22,7 +22,7 @@ class GarbageCollection
     public static void update (int[] speicher)
     {
     	int n = (int)(Math.random() * 10);
-        speicher[n] = (int)(Math.random() * 9);
+        speicher[n] = (int)(Math.random() * 11);
         if (speicher[n] == n) speicher[n] = -1;
     }
     
